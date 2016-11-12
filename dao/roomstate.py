13f318fe894daf20b622eeaa90faff4775c18ff2
mateuszkawa <m.kawa@op.pl>
2016-11-12 22:57:24 +0100

    added collision checking mechanism

--- dao/roomstate.py
+++ dao/roomstate.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import Column, Integer, Date, ForeignKey
+from sqlalchemy import Column, Integer, Date, ForeignKey, or_, and_
 from dao import Base, get_engine
 from sqlalchemy.orm import create_session
 import datetime
@@ -15,13 +15,32 @@ class RoomState(Base):
     @staticmethod
     def get_all_states_for_room(room_id: Integer) -> 'RoomState':
         session = create_session(bind=get_engine())
-        room = session.query(RoomState).filter(RoomState.room == room_id).all()
+        states = session.query(RoomState).filter(RoomState.room == room_id).all()
         session.close()
-        return room
+        return states
 
     @staticmethod
     def get_all_states_for_room_after(room_id: Integer, date_after: datetime.date) -> 'RoomState':
         session = create_session(bind=get_engine())
-        room = session.query(RoomState).filter(RoomState.room == room_id, RoomState.reserved_to < date_after).all()
+        states = session.query(RoomState).filter(RoomState.room == room_id, RoomState.reserved_to < date_after).all()
         session.close()
-        return room
+        return states
+
+    @staticmethod
+    def check_colliding_states_for_room(room_id: Integer, date_start: datetime.date, date_end: datetime.date):
+        session = create_session(bind=get_engine())
+        states = session.query(RoomState).filter(
+            RoomState.room == room_id
+        ).filter(
+            or_(
+                and_(RoomState.reserved_from > date_start, RoomState.reserved_from < date_end),
+                and_(RoomState.reserved_to > date_start, RoomState.reserved_to < date_end),
+                and_(RoomState.reserved_from > date_start, RoomState.reserved_to < date_end),
+                and_(RoomState.reserved_from < date_start, RoomState.reserved_to > date_end))).all()
+        session.close()
+        return states
+
+
+if __name__ == '__main__':
+    for state in RoomState.check_colliding_states_for_room(3, datetime.date(2016, 11, 12), datetime.date(2016, 11, 14)):
+        print("from: %s, to: %s" % (state.reserved_from, state.reserved_to))
